fca7870c2096f0cc1d8d2a809788b81c2d15d487
Nikechukwu <nikechukwu@gmail.com>
2024-04-21 17:46:03 -0400

    QuetzalJS Code

---
+++ packages/quetzal-shared/src/quetzal-shared.ts
@@ -0,0 +1,10 @@
+declare class QuetzalBaseClass {};
+
+export function QuetzalExtElement(constructor: {
+    new(): HTMLElement,
+    prototype: HTMLElement
+}) {
+    return class extends constructor implements QuetzalBaseClass {
+        
+    }
+}
